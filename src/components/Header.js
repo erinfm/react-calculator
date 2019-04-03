23cc85586c7b086871fd27aa14b38a6334d49c83
Erin M <erinfranmc@gmail.com>
2019-04-03 14:20:27 +0100

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="headerBar">
+      <h1 className="headerText">React Calculator</h1>
+    </div>
+  );
+};
+
+export default Header;
