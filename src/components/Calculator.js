a421c2a314835bb9b5228188e8f9514f9bf404e9
Erin M <erinfranmc@gmail.com>
2019-04-03 15:36:15 +0100

    added basic styling

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,43 @@
 import React from "react";
 import "./Calculator.css";
+import Input from "./Input";
 import Button from "./Button";
 
 const Calculator = () => {
   return (
     <div className="calcContainer">
-      <div className="calcRow">
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
+      <div className="calcRowInput">
+        <Input input={"input field"} />
       </div>
-      <div className="calcRow">
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
+      <div className="calcRowBtn">
+        <Button>C</Button>
+        <Button>÷</Button>
+        <Button>×</Button>
+        <Button> </Button>
       </div>
-      <div className="calcRow">
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
+      <div className="calcRowBtn">
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
+        <Button>−</Button>
       </div>
-      <div className="calcRow">
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
+      <div className="calcRowBtn">
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button>
+        <Button>+</Button>
       </div>
-      <div className="calcRow">
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
-        <Button>A</Button>
+      <div className="calcRowBtn">
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button>
+        <Button>±</Button>
+      </div>
+      <div className="calcRowBtn">
+        <Button> </Button>
+        <Button>0</Button>
+        <Button>.</Button>
+        <Button>=</Button>
       </div>
     </div>
   );
---
+++ src/components/Input.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Input.css";
+
+const Input = props => {
+  return <div className="inputStyle">{props.input}</div>;
+};
+
+export default Input;
